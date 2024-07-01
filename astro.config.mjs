4601fcf266c55b1c777c645b8b99af7164458224
celina garcia <code@koral.one>
2024-07-01 00:31:50 -0700

    add mdx integration, remove unusued plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,35 +3,12 @@ import mdx from "@astrojs/mdx";
 import remarkDirective from "remark-directive";
 import sectionizePlugin from "./sectionizePlugin";
 import directive from "./src/plugins/directive";
-import directiveHighlight from "./src/plugins/Highlight";
-import directiveBreadcrumb from "./src/plugins/Breadcrumb";
-import directiveTabs from "./src/plugins/Tabs";
-import directiveFeatureCard from "./src/plugins/FeatureCard";
-import directiveRowColumnLayout from "./src/plugins/RowColumnLayout";
 import nestedDirective from "./src/plugins/NestedDirective";
-import fullWidthDirective from "./src/plugins/FullWidthDirective";
-import {
-  numberedListDirective,
-  numberedItemDirective,
-} from "./src/plugins/NumberedItem";
 
 export default defineConfig({
   integrations: [mdx()],
   markdown: {
-    remarkPlugins: [
-      remarkDirective,
-      directive,
-      sectionizePlugin,
-      directiveHighlight,
-      directiveBreadcrumb,
-      directiveTabs,
-      directiveFeatureCard,
-      directiveRowColumnLayout,
-      nestedDirective,
-      fullWidthDirective,
-      numberedItemDirective,
-      numberedListDirective,
-    ],
+    remarkPlugins: [remarkDirective, directive, nestedDirective],
     rehypePlugins: [],
   },
 });
--- sectionizePlugin.js
+++ sectionizePlugin.js
@@ -1,17 +1,16 @@
 import { visit } from "unist-util-visit";
 import slugify from "slugify";
 import { findAfter } from "unist-util-find-after";
-const MAX_HEADING_DEPTH = 2;
+
+const HEADING_DEPTH = 2;
 
 function sectionizePlugin() {
   return (tree) => {
-    for (let depth = MAX_HEADING_DEPTH; depth > 0; depth--) {
-      visit(
-        tree,
-        (node) => node.type === "heading" && node.depth === depth,
-        sectionize
-      );
-    }
+    visit(
+      tree,
+      (node) => node.type === "heading" && node.depth === HEADING_DEPTH,
+      sectionize
+    );
   };
 }
 
@@ -20,7 +19,7 @@ function sectionize(node, index, parent) {
   const startIndex = index;
   const depth = start.depth;
 
-  const isEnd = (node) => node.type === "heading" && node.depth <= depth;
+  const isEnd = (node) => node.type === "heading" && node.depth === depth;
   const end = findAfter(parent, start, isEnd);
   const endIndex = parent.children.indexOf(end);
 
